e1928ebfc4e8113d0e95be4ea5582c292fcb470f
afeezdev <afeezdev@gmail.com>
2023-11-04 13:12:55 +0000

    flask templates

--- server.py
+++ server.py
@@ -4,16 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_work():
-    return 'Hello, Afeez!, How '
+    return render_template('./index.html')
 
 @app.route('/blog')
 def blog():
-    return 'Hello, these are my thoughts on blogs'
+    return render_template('./index2.html')
 
 @app.route('/blog/2022/dog')
 def blog2():
     return 'This is my dog'
 
-a = 2
-a = 3
-print(a)
